5257d52b723a14832811616373608a920b75e722
Aditya Kumar <leo03.akrs@gmail.com>
2024-04-01 12:17:43 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,23 @@
-// vite.config.js
+// // vite.config.js
+// import { defineConfig } from "vite";
+// import { resolve } from "path";
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, "index.html"), // Your main entry point (e.g., home page)
+//         about: resolve(__dirname, "about.html"), // Additional HTML pages
+//         contact: resolve(__dirname, "contact.html"),
+//         products: resolve(__dirname, "products.html"),
+//         addToCart: resolve(__dirname, "addToCart.html"),
+//         // Add more entry points for other HTML files as needed
+//       },
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
@@ -14,5 +33,14 @@ export default defineConfig({
         // Add more entry points for other HTML files as needed
       },
     },
+  },
+  css: {
+    // Include styles.css
+    include: [
+      // Relative path to your styles.css file
+      "styles.css",
+    ],
+    // PostCSS plugins, options
+    // https://vitejs.dev/config/#css-postcssplugins
   },
 });
